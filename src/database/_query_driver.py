9ad8400ca6f57dbabd63d6f3ef708445a7e28433
alias-ktc <alenpeni.info@gmail.com>
2026-02-09 17:04:08 +1000

    add-docs: QueryDriver
    Added documentation on the purpose of the QueryDriver class and it's
    methods

--- src/database/_query_driver.py
+++ src/database/_query_driver.py
@@ -1,22 +1,33 @@
+from collections.abc import Sequence
+
 from sqlmodel import Session, SQLModel, create_engine, select
 
 from .models import Project
 
 
 class QueryDriver:
-    """
-    Sets up an engine to the database and expose common
-    query operations
-    """
+    """QueryDriver to facilitate database connection and query operations"""
 
     # TODO: Improve documentation
     # TODO: Check if SQLModel has an async engine
 
     def __init__(self, db_path: str) -> None:
+        """
+        Connect to database and create tables
+
+        Args:
+            db_path (str): path to the database
+        """
         self._engine = create_engine(db_path)
         SQLModel.metadata.create_all(self._engine)
 
-    def get_all_projects(self):
+    def get_all_projects(self) -> Sequence[Project]:
+        """
+        Instantiates a session to fetch all projects closed upon function return
+
+        Returns:
+            Sequence[Project]: Snapshot of all the projects during function call
+        """
         with Session(self._engine) as session:
             return session.exec(select(Project)).all()
 
@@ -25,3 +36,6 @@ class QueryDriver:
             first_project = Project(description="This is a Project")
             session.add(first_project)
             session.commit()
+
+    def add_project(self) -> None:
+        pass
