84fcfa209c3d697d17908c5874f5a5ae98b11c3e
alias-ktc <alenpeni.info@gmail.com>
2026-02-10 18:44:28 +1000

    added: DTO for Project
    Create and Select schemas were created to help decouple the UI from the
    Database.

--- src/database/models/_projects.py
+++ src/database/models/_projects.py
@@ -3,10 +3,25 @@ from sqlmodel import Field
 from ._helpers import BaseSchema
 
 
+class ProjectCreate(BaseSchema):
+    """DTO from TUI to QueryDriver."""
+
+    pass
+
+
+class ProjectSelect(BaseSchema):
+    """DTO from QueryDriver to TUI.
+
+    All Projects stored in the database HAVE primary keys
+    """
+
+    id: int = Field(primary_key=True)
+
+
 class Project(BaseSchema, table=True):
     """Project entity.
 
-    This is the main entity of interest and OWNS tasks and milestones
+    This is the main entity of interest and HAS tasks and milestones
     """
 
     # TODO: Fill in using the apporpriate columns
