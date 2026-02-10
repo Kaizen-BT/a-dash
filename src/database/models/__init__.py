fabe27d3f5ff53572f96db3db673feffd7d12f5e
alias-ktc <alenpeni.info@gmail.com>
2026-02-10 19:25:08 +1000

    modified-exports: Database.Models
    Added the Select and Create schemas to the exports of the Models
    subpackage

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -4,8 +4,18 @@ This package contains the definitions of each of the tables/Models
 used within the application
 """
 
-from ._milestones import Milestone
-from ._projects import Project
-from ._tasks import Task
+from ._milestones import Milestone, MilestoneCreate, MilestoneSelect
+from ._projects import Project, ProjectCreate, ProjectSelect
+from ._tasks import Task, TaskCreate, TaskSelect
 
-__all__ = ["Project", "Milestone", "Task"]
+__all__ = [
+    "Project",
+    "Milestone",
+    "Task",
+    "ProjectSelect",
+    "MilestoneSelect",
+    "TaskSelect",
+    "ProjectCreate",
+    "MilestoneCreate",
+    "TaskCreate",
+]
