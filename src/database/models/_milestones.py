397c81fae3aa07397da42a5660386c7300ecb1c7
alias-ktc <alenpeni.info@gmail.com>
2026-02-10 19:16:57 +1000

    added: DTO for Milestones and Tasks
    DTO's added for both Milestones and Tasks

--- src/database/models/_milestones.py
+++ src/database/models/_milestones.py
@@ -3,6 +3,21 @@ from sqlmodel import Field
 from ._helpers import BaseSchema
 
 
+class MilestoneCreate(BaseSchema):
+    """DTO from TUI to QueryDriver."""
+
+    pass
+
+
+class MilestoneSelect(BaseSchema):
+    """DTO from QueryDriver to TUI.
+
+    All Milestones stored in the database HAVE primary keys
+    """
+
+    id: int = Field(primary_key=True)
+
+
 class Milestone(BaseSchema, table=True):
     """Milestone entity.
 
